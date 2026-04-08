63c6925fe0ad63f94c67c33527897552f7ff2a0e
Caique Alves <caiquealves@icloud.com>
2026-04-08 00:53:48 -0300

    Add files via upload

---
+++ Produto/Produto.cs
@@ -0,0 +1,29 @@
+﻿class Produto
+{
+    public string Nome { get; set; }
+    private int quantidadeEstoque;
+
+    public Produto(string nome, int qtdEstoque)
+    {
+        Nome = nome;
+        quantidadeEstoque = qtdEstoque;
+    }
+
+    public void Retirar(int quantidade)
+    {
+        if (quantidadeEstoque > quantidade)
+        {
+            quantidadeEstoque -= quantidade;
+            Console.WriteLine("Você retirou {0} {1}!", quantidade, Nome);
+        }
+        else
+        {
+            Console.WriteLine("Erro: Não foi possível retirar {0} {1}, pois temos apenas {2} em estoque!", quantidade, Nome, quantidadeEstoque);
+        }
+    }
+
+    public void ExibirEstoque()
+    {
+        Console.WriteLine("\nEstoque atual: {0} {1}", quantidadeEstoque, Nome);
+    }
+}
---
+++ Produto/Teste.cs
@@ -0,0 +1,5 @@
+﻿Produto produto = new Produto("Celulares", 30);
+produto.ExibirEstoque();
+produto.Retirar(10);
+produto.ExibirEstoque();
+produto.Retirar(25);
